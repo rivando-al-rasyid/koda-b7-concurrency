de48eb3af6eb10f5e597e561cbb7957325e0d38d
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-29 23:52:43 +0700

    feat ; ask input for number

--- internals/square/generate.go
+++ internals/square/generate.go
@@ -1,8 +1,28 @@
 package square
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func Generatenum(ch chan<- int) {
 	defer close(ch)
-	for i := 1; i <= 10; i++ {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("input angka: ")
+	scanner.Scan()
+	n := strings.TrimSpace(scanner.Text())
+
+	num, err := strconv.Atoi(n)
+	if err != nil {
+		fmt.Println("Error: input must be a number")
+		return
+	}
+
+	for i := 1; i <= num; i++ {
 		ch <- i
 	}
 }
